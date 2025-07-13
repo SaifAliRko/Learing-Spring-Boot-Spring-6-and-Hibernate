974fb4ff1983e3532d908c31f99a4babd663f735
Saif Ali <saif.ali96587@gmail.com>
2025-07-13 23:32:45 +0200

    tested primary key setup

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/luv2code/util/TennisCoach.java
@@ -0,0 +1,12 @@
+package com.luv2code.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+  @Override
+  public String getDailyWorkout() {
+    return "Practice your backhand volley for 30 minutes.";
+  }
+}
