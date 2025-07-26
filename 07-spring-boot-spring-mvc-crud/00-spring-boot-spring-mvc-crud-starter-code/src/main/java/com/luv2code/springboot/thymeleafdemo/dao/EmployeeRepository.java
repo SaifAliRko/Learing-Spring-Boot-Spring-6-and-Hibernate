793c3841667387d489cab39bfb16e3165d73cbaa
Saif Ali <saif.ali96587@gmail.com>
2025-07-26 08:44:45 +0200

    Created a crub mvc thymeleaf app

---
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.luv2code.springboot.thymeleafdemo.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+  // Add a method to sort an employee by last name
+  public List<Employee> findAllByOrderByLastNameAsc();
+
+}
--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/test/java/com/luv2code/springboot/t/CruddemoApplicationTests.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/test/java/com/luv2code/springboot/t/CruddemoApplicationTests.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ThymeleafDemoApplicationTests {
 
-  @Test
-  void contextLoads() {}
+	@Test
+	void contextLoads() {
+	}
+
 }
